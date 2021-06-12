ddea36703a444143db36aeea4e145d8ad95fcc24
Andrew Patterson <andrew.patterson747@gmail.com>
2021-06-11 20:03:26 -0500

    Model Developer

---
+++ models/model_b.rb
@@ -0,0 +1,11 @@
+developer = {
+  'name' => "Andrew",
+  'gender' => "Male",
+  'weight' => 165,
+  'height' => 70,
+  'Texan' => true,
+  'Climber' => true,
+
+  'favorite_foods' => ["mango, pizza, prosciutto"],
+  'hates' => ["Movie talkers", "drama", "jerks"]
+}
