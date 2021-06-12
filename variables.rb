951c063faa47372470c8dc2c14f6780e1252835d
Andrew Patterson <andrew.patterson747@gmail.com>
2021-06-11 19:04:53 -0500

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+movie = "Zombieland"
+is_entertaining = true
+movie = "The Room"
+num_4 = 4
+num_6 = 6
+product = num_4 * num_6
+favorite_foods = ["mango", "pizza", "prosciutto"]
+favorite_foods.shift(1)
+puts movie.length
