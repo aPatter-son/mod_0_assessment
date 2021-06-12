151e3a7660ca7935bf955f92bab20908d4e26d37
Andrew Patterson <andrew.patterson747@gmail.com>
2021-06-11 19:33:24 -0500

    Model dinosaurs

---
+++ models/model_a.rb
@@ -0,0 +1,39 @@
+
+name = {
+  'T rex' => "Tyrannosaurus rex",
+  'Stego' => "Stegosaurus"
+}
+
+diet = {
+  'T rex' => "Carnivore",
+  'Stego' => "Herbivore"
+}
+
+amount_of_legs = {
+  'T rex' => 2,
+  'Stego' => 4
+}
+
+length = {
+  'T rex' => 40,
+  'Stego' => 30
+}
+
+lived_in_jurassic = {
+  'T rex' => false,
+  'Stego' => true
+
+has_gills = {
+  'T rex' => false,
+  'Stego' => false
+}
+
+fossil_locations = [
+  {'T rex' => "North America"}
+  {'Stego' => "Western Europe"}
+]
+
+colors = [
+  {'T rex' => "reddish-brown"}
+  {'Stego' => "muted shades of green and brown"}
+]
